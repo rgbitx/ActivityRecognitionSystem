a498dc320fbf4c6d93ebd60e11633774a802e7f1
qiuhai <rgbitx@163.com>
2016-10-27 14:34:02 +0800

    calculate measures

--- app/src/main/java/com/example/qiuhai/activityrecognitionsystem/MainActivity.java
+++ app/src/main/java/com/example/qiuhai/activityrecognitionsystem/MainActivity.java
@@ -9,12 +9,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     Button btn_start, btn_stop;
     private SensorManager sensorManager;
-    private MySensorEventListener sensorEventListener;
     Sensor accSensor, gyroSensor, magSensor;
+
+    ArrayList<float[]> accBuffer;
+    int bufferSize = 500;
+    int calDelay;
+    boolean isCal;
+    int count;
 
     float[] acc;
     float[] gyro;
@@ -28,7 +35,11 @@ public class MainActivity extends AppCompatActivity {
         btn_start = (Button) findViewById(R.id.btn_start);
         btn_stop = (Button) findViewById(R.id.btn_stop);
 
-        sensorEventListener = new MySensorEventListener();
+        accBuffer = new ArrayList<>();
+        count = 0;
+        calDelay = 0;
+        isCal = false;
+
         //get sensor manage
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
@@ -37,12 +48,24 @@ public class MainActivity extends AppCompatActivity {
         magSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
 
+
         btn_start.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
+                isCal = true;
 
+                sensorManager.registerListener(MainActivity.this, accSensor, SensorManager.SENSOR_DELAY_GAME);
+                sensorManager.registerListener(MainActivity.this, gyroSensor, SensorManager.SENSOR_DELAY_GAME);
+                sensorManager.registerListener(MainActivity.this, magSensor, SensorManager.SENSOR_DELAY_GAME);
 
+            }
+        });
 
+        btn_stop.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                isCal = false;
+                sensorManager.unregisterListener(MainActivity.this);
             }
         });
 
@@ -50,41 +73,125 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(sensorEventListener, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(sensorEventListener, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(sensorEventListener, magSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(sensorEventListener);
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER)
+        {
+            acc = event.values;
+
+            // control the calculation
+            if(isCal) {
+                //put the data into the buffer
+                if(count < bufferSize) {
+                    accBuffer.add(acc);
+                } else {
+                    accBuffer.remove(0);
+                    accBuffer.add(acc);
+                }
+                count++;
+
+                calDelay++;
+                // every 50, calculate the measure once
+                if(calDelay % 50 == 0) {
+
+
+
+
+
+                }
+
+            }
+
+
+        }
+        if(event.sensor.getType()==Sensor.TYPE_GYROSCOPE) {
+            gyro = event.values;
+        }
+        if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD) {
+            mag = event.values;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
 
-    private final class MySensorEventListener implements SensorEventListener
-    {
 
-        @Override
-        public void onSensorChanged(SensorEvent event)
-        {
-            if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER)
-            {
-                acc = event.values;
-            }
-            if(event.sensor.getType()==Sensor.TYPE_GYROSCOPE) {
-                gyro = event.values;
-            }
-            if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD) {
-                mag = event.values;
-            }
 
+
+    private void mean(ArrayList<float[]> input, float[] results) {
+        if(input != null && input.size()>0 && input.get(0).length == 3) return;
+
+        float[] sum = new float[3];
+
+        for(int CH = 0; CH < 3; CH++) {
+            for(int i=0; i < input.size(); i++){
+                sum[CH] += input.get(i)[CH];
+            }
+            results[CH] = sum[CH] / input.size();
         }
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy)
-        {
-
-        }
     }
+
+    private void std(ArrayList<float[]> input, float[] mean, float[] results) {
+        if(input != null && input.size()>0 && input.get(0).length == 3
+                && mean != null && mean.length == 3) return;
+
+        float[] sum = new float[3];
+
+        for(int CH = 0; CH < 3; CH++) {
+            for(int i=0; i < input.size(); i++){
+                sum[CH] += (input.get(i)[CH] - mean[CH]) * (input.get(i)[CH] - mean[CH]);
+            }
+            results[CH] = (float) Math.sqrt(sum[CH] / input.size());
+        }
+
+    }
+
+    private void max(ArrayList<float[]> input, float[] results) {
+        if(input != null && input.size()>0 && input.get(0).length == 3) return;
+
+        float[] max = new float[3];
+
+        for(int CH = 0; CH < 3; CH++) {
+            max[CH] = Integer.MIN_VALUE;
+            for(int i=0; i < input.size(); i++){
+                if(input.get(i)[CH] > max[CH]) {
+                    max[CH] = input.get(i)[CH];
+                }
+
+            }
+            results[CH] = max[CH];
+        }
+
+    }
+
+    private void min(ArrayList<float[]> input, float[] results) {
+        if(input != null && input.size()>0 && input.get(0).length == 3) return;
+
+        float[] min = new float[3];
+
+        for(int CH = 0; CH < 3; CH++) {
+            min[CH] = Integer.MAX_VALUE;
+            for(int i=0; i < input.size(); i++){
+                if(input.get(i)[CH] < min[CH]) {
+                    min[CH] = input.get(i)[CH];
+                }
+
+            }
+            results[CH] = min[CH];
+        }
+
+    }
+
 
 }
