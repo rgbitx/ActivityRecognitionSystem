d856baf29185abd660aa25246ba5bd1671d1a06e
qiuhai <rgbitx@163.com>
2016-10-27 16:59:55 +0800

    knn model 1st

--- app/src/main/java/com/example/qiuhai/activityrecognitionsystem/MainActivity.java
+++ app/src/main/java/com/example/qiuhai/activityrecognitionsystem/MainActivity.java
@@ -9,7 +9,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import weka.classifiers.Classifier;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -27,6 +35,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     float[] gyro;
     float[] mag;
 
+    ArrayList<Attribute> atts;
+    ArrayList<String> classVal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +51,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         calDelay = 0;
         isCal = false;
 
+        // Model initialization
+        atts = new ArrayList<>();
+        classVal = new ArrayList<>();
+
+        // set the attributions of the instance
+        setInstanceAttributions(atts, classVal);
+
+
         //get sensor manage
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
@@ -52,11 +71,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         btn_start.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
-                isCal = true;
 
                 sensorManager.registerListener(MainActivity.this, accSensor, SensorManager.SENSOR_DELAY_GAME);
-                sensorManager.registerListener(MainActivity.this, gyroSensor, SensorManager.SENSOR_DELAY_GAME);
-                sensorManager.registerListener(MainActivity.this, magSensor, SensorManager.SENSOR_DELAY_GAME);
+//                sensorManager.registerListener(MainActivity.this, gyroSensor, SensorManager.SENSOR_DELAY_GAME);
+//                sensorManager.registerListener(MainActivity.this, magSensor, SensorManager.SENSOR_DELAY_GAME);
+
+                isCal = true;
 
             }
         });
@@ -94,18 +114,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 } else {
                     accBuffer.remove(0);
                     accBuffer.add(acc);
+                    calDelay++;
+
+                    // every 50, calculate the measure once
+                    if(calDelay % 50 == 0) {
+                        float[] mean = mean(accBuffer);
+                        float[] std = std(accBuffer, mean);
+                        float[] max = max(accBuffer);
+                        float[] min = min(accBuffer);
+
+                        // new an instance
+                        Instances measureInstance = new Instances("MeasureInstances", atts, 0);
+                        measureInstance.setClassIndex(12);
+
+                        // set measurement to instance values
+                        double[] instanceValue = new double[12];
+
+                        // set measure value to each attribution in the instance
+                        setInstanceAttributionValues(instanceValue,mean,std,max,min);
+
+                        measureInstance.add(new DenseInstance(1.0,instanceValue));
+
+                    }
+
                 }
                 count++;
-
-                calDelay++;
-                // every 50, calculate the measure once
-                if(calDelay % 50 == 0) {
-
-
-
-
-
-                }
 
             }
 
@@ -126,12 +159,79 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
 
+    private void setInstanceAttributions(ArrayList<Attribute> atts, ArrayList<String> classVal) {
+        classVal.add("A");
+        classVal.add("B");
+        classVal.add("C");
+        classVal.add("E");
+
+        atts.add(new Attribute("F1"));
+        atts.add(new Attribute("F2"));
+        atts.add(new Attribute("F3"));
+        atts.add(new Attribute("F4"));
+        atts.add(new Attribute("F5"));
+        atts.add(new Attribute("F6"));
+        atts.add(new Attribute("F7"));
+        atts.add(new Attribute("F8"));
+        atts.add(new Attribute("F9"));
+        atts.add(new Attribute("F10"));
+        atts.add(new Attribute("F11"));
+        atts.add(new Attribute("F12"));
 
 
-    private void mean(ArrayList<float[]> input, float[] results) {
-        if(input != null && input.size()>0 && input.get(0).length == 3) return;
+        // CLASS  1
+        atts.add(new Attribute("class", classVal));
+
+    }
+
+    private void setInstanceAttributionValues(double[] instanceValue,
+                                              float[] mean, float[] std,
+                                              float[] max, float[] min) {
+
+        instanceValue[0] = mean[0];
+        instanceValue[1] = std[0];
+        instanceValue[2] = max[0];
+        instanceValue[3] = min[0];
+        instanceValue[4] = mean[1];
+        instanceValue[5] = std[1];
+        instanceValue[6] = max[1];
+        instanceValue[7] = min[1];
+        instanceValue[8] = mean[2];
+        instanceValue[9] = std[2];
+        instanceValue[10] = max[2];
+        instanceValue[11] = min[2];
+
+        // class 1
+        instanceValue[12] = 0; // CLASS
+
+    }
+
+    //load model
+    private void activityRegModel(String loadModelName, Instances measureInstance) throws Exception {
+
+        InputStream is = this.getAssets().open(loadModelName);
+        ObjectInputStream ois = new ObjectInputStream(is);
+        Classifier cls = (Classifier) ois.readObject();
+        ois.close();
+
+        int pos = 0;
+        if(cls == null) {
+            return;
+        }
+
+        double value = cls.classifyInstance(measureInstance.instance(pos));
+
+
+    }
+
+
+
+    private float[] mean(ArrayList<float[]> input) {
+        if(input == null || input.size() == 0 || input.get(0).length != 3)
+            return null;
 
         float[] sum = new float[3];
+        float[] results = new float[3];
 
         for(int CH = 0; CH < 3; CH++) {
             for(int i=0; i < input.size(); i++){
@@ -140,13 +240,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             results[CH] = sum[CH] / input.size();
         }
 
+        return results;
+
     }
 
-    private void std(ArrayList<float[]> input, float[] mean, float[] results) {
-        if(input != null && input.size()>0 && input.get(0).length == 3
-                && mean != null && mean.length == 3) return;
+    private float[] std(ArrayList<float[]> input, float[] mean) {
+        if(input == null || input.size() == 0 || input.get(0).length != 3
+                || mean == null || mean.length != 3) return null;
 
         float[] sum = new float[3];
+        float[] results = new float[3];
 
         for(int CH = 0; CH < 3; CH++) {
             for(int i=0; i < input.size(); i++){
@@ -155,10 +258,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             results[CH] = (float) Math.sqrt(sum[CH] / input.size());
         }
 
+        return results;
+
     }
 
-    private void max(ArrayList<float[]> input, float[] results) {
-        if(input != null && input.size()>0 && input.get(0).length == 3) return;
+    private float[] max(ArrayList<float[]> input) {
+        if(input == null || input.size() == 0 || input.get(0).length != 3)
+            return null;
 
         float[] max = new float[3];
 
@@ -170,13 +276,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 }
 
             }
-            results[CH] = max[CH];
         }
+
+        return max;
 
     }
 
-    private void min(ArrayList<float[]> input, float[] results) {
-        if(input != null && input.size()>0 && input.get(0).length == 3) return;
+    private float[] min(ArrayList<float[]> input) {
+        if(input == null || input.size() == 0 || input.get(0).length != 3)
+            return null;
 
         float[] min = new float[3];
 
@@ -188,8 +296,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 }
 
             }
-            results[CH] = min[CH];
         }
+
+        return min;
 
     }
 
